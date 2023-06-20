a3c877b2ca84d18f0bb6e571cded511fa7fb63d3
VasiliiChi <chibisowasya@gmail.com>
2023-06-20 11:44:33 +0300

    Refactoring

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -33,7 +33,7 @@ namespace LifeSpot
                     var html = await File.ReadAllTextAsync(viewPath);
                     await context.Response.WriteAsync(html);
                 });
-               
+
                 endpoints.MapGet("/Static/CSS/index.css", async context =>
                 {
                     // по аналогии со страницей Index настроим на нашем сервере путь до страницы со стилями, чтобы браузер знал, откуда их загружать
