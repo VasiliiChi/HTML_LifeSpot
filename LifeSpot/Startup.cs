e64cda9445900526223e280099f5c0abec1f7b0a
VasiliiChi <chibisowasya@gmail.com>
2023-07-01 14:24:53 +0300

    HTML, CSS, JS update
    Реализовали переход между 2 страницами html;
    
    Подключили к ним Css;

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -10,21 +10,17 @@ namespace LifeSpot
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseRouting();
+
             // Загружаем отдельные элементы для вставки в шаблон: боковое меню и футер
             string footerHtml =
                 File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "Footer.html"));
@@ -35,7 +31,7 @@ namespace LifeSpot
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "Index.html");
+                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
 
                     // Загружаем шаблон страницы, вставляя в него элементы
                     var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
@@ -59,7 +55,6 @@ namespace LifeSpot
 
                 endpoints.MapGet("/Static/Css/Index.css", async context =>
                 {
-                    // по аналогии со страницей Index настроим на нашем сервере путь до страницы со стилями, чтобы браузер знал, откуда их загружать
                     var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "Css", "Index.css");
                     var css = await File.ReadAllTextAsync(cssPath);
                     await context.Response.WriteAsync(css);
@@ -67,7 +62,6 @@ namespace LifeSpot
 
                 endpoints.MapGet("/Static/Js/Index.js", async context =>
                 {
-                    // по аналогии со страницей Index настроим на нашем сервере путь до файла Java Script
                     var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "Js", "Index.js");
                     var js = await File.ReadAllTextAsync(jsPath);
                     await context.Response.WriteAsync(js);
--- LifeSpot/Static/Js/Index.js
+++ LifeSpot/Static/Js/Index.js
@@ -1,7 +1,10 @@
-    let age = prompt('Пожалуйста, введите ваш возраст');
-    if (age >= 18) {
+let age = prompt('Пожалуйста, введите ваш возраст');
+if (age >= 18) {
     alert('Приветствуем на LifeSpot ' + new Date().toLocaleString());
 } else {
     alert('Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены')
     window.location = 'https://www.google.com/';
 }
+
+let userName = prompt('Пожалуйста, введите свое имя');
+alert (`Приветствуем, ${userName}. В Вашем имени ${userName.length} символов.`)
