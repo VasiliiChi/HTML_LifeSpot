b9ff6e77b3b009d96258653118e8f23cc188aaeb
VasiliiChi <chibisowasya@gmail.com>
2023-07-02 23:43:10 +0300

    Update Index.js

--- LifeSpot/Static/Js/Index.js
+++ LifeSpot/Static/Js/Index.js
@@ -1,17 +1,30 @@
-let age = prompt('Пожалуйста, введите ваш возраст');
-if (age >= 18) {
-    alert('Приветствуем на LifeSpot ' + new Date().toLocaleString());
+// Создадим объект для хранения сессии
+let session = new Map();
+// Сохраним userAgent
+session.set("userAgent", window.navigator.userAgent);
+
+// Запросим и сохраним возраст пользователя
+session.set("age", prompt("Пожалуйста, введите Ваш возраст"));
+
+// Проверка возраста и сохранение сессии
+if (session.get("age") >= 18) {
+    let startDate = new Date().toLocaleString()
+    alert('Приветствуем на LifeSpot ' + '\n' + "Текущее время: " + startDate);
+    session.set("startDate", startDate);
 } else {
     alert('Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены')
     window.location = 'https://www.google.com/';
+    a = true + 20 + "name";
 }
 
-let userName = prompt('Пожалуйста, введите свое имя');
-alert(`Приветствуем, ${userName}. В Вашем имени ${userName.length} символов.`);
+// Вывод в консоль
+for (let result of session) {
+    console.log(result);
+   }
 
 function filterContent() {
     let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
-       // Находим контейнеры с видео, которые необходимо фильтровать
+    // Находим контейнеры с видео, которые необходимо фильтровать
     let elements = document.getElementsByClassName('video-container');
 
     for (let i = 0; i <= elements.length; i++) {
