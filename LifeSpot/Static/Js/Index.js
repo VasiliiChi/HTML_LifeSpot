875afe5ea08c60799151ba76e446c2988d7d141f
VasiliiChi <chibisowasya@gmail.com>
2023-07-02 18:24:21 +0300

    Add search
    Реализован поиск по названию видео на главной странице

--- LifeSpot/Static/Js/Index.js
+++ LifeSpot/Static/Js/Index.js
@@ -7,4 +7,22 @@ if (age >= 18) {
 }
 
 let userName = prompt('Пожалуйста, введите свое имя');
-alert (`Приветствуем, ${userName}. В Вашем имени ${userName.length} символов.`);
+alert(`Приветствуем, ${userName}. В Вашем имени ${userName.length} символов.`);
+
+function filterContent() {
+    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+       // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].querySelector('.video-title').innerText;
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+            // Описание
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
