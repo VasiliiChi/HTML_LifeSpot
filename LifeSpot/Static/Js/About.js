1572f13929db0a4af2eaba934b6582483ec1d5e3
Vasilii <chibisowasya@gmail.com>
2023-07-17 21:50:30 +0300

    Use Prototype
    Добавили возможность оставлять отзывы с рейтингом используя прототип

--- LifeSpot/Static/Js/About.js
+++ LifeSpot/Static/Js/About.js
@@ -1,22 +1,39 @@
-function getReview() {
+function getComment() {
     // Создадим объект
-    let review = {}
+    let comment = {}
 
     // Сохраним свойство имени
-    review["userName"] = prompt("Как вас зовут ?")
-    if (review["userName"] == null) {
+    comment.author = prompt("Как вас зовут ?")
+    if (comment.author == null) {
         return
     }
 
     // Сохраним текст отзыва
-    review["comment"] = prompt("Напишите свой отзыв")
-    if (review["comment"] == null) {
+    comment.text = prompt("Напишите ваш отзыв")
+    if (comment.text == null) {
         return
     }
 
     // Сохраним текущее время
-    review["date"] = new Date().toLocaleString()
+    comment.date = new Date().toLocaleString()
 
-    // Добавим на страницу
-    writeReview(review)
+    // Запросим у пользователя обычный ли это текст или комментарий
+    let enibleLikes = confirm("Разрешить пользователям оценивать Ваш отзыв?");
+    
+    if (enibleLikes){
+        // Создадим для отзыва новый объект из прототипа - комментария
+        let review = Object.create(comment);
+        // Добавим ему нужное свойство
+        review.rate = 0;
+
+        // Добавляем отзыв с возможностью пользовательских оценок
+        writeReview(review);
+    }
+    else {
+        // Добавим простой комментарий без возможности оценки
+        writeReview(comment);
+    }
+  
+    
+   
 }
